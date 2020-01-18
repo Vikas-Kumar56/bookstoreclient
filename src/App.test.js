2b39fdc2bd5f24be9cb71e1a8e402c3982991442
Vikas Kumar <vikas.kumar1@thoughtworks.com>
2020-01-18 22:26:15 +0530

    cicd setup

--- src/App.test.js
+++ src/App.test.js
@@ -5,6 +5,6 @@ import App from "./App";
 describe("App component", () => {
   it("should render app without error", () => {
     const { asFragment } = render(<App />);
-    expect(asFragment).toMatchSnapshot();
+    expect(asFragment()).toMatchSnapshot();
   });
 });
