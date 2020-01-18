b60e061e8a0a31b55c39b602e6c551b384aaea83
Vikas Kumar <vikas.kumar1@thoughtworks.com>
2020-01-18 23:57:19 +0530

    cicd setup and deploy to heroku

--- src/App.test.js
+++ src/App.test.js
@@ -3,7 +3,7 @@ import { render } from "@testing-library/react";
 import App from "./App";
 
 describe("App component", () => {
-  it("should render app without error", () => {
+  it("should render app with error", () => {
     const { asFragment } = render(<App />);
     expect(asFragment()).toMatchSnapshot();
   });
